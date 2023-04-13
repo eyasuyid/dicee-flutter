7bb0db2c67f09479bd5eaba024f3056861b0457a
eyasuyid <eyasuyid@gmail.com>
2023-04-13 11:07:18 +0300

    button listen works 😊

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,35 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  print("Left button clicked!");
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextButton(
+                  onPressed: () {
+                    print("Left button clicked!");
+                  },
+                  child: Image.asset('images/dice1.png'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
